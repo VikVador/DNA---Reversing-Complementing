478cb5c6a7bf816eb919a9a852e90aa57f35c883
Victor Mangeleer <vmangeleer@student.uliege.be>
2022-08-04 19:42:13 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
-#------------------------------------
+#-------------------------------------
 #
-#    DNA - Reverse & Complementing
+#    DNA - Reversing & Complementing
 #
-#------------------------------------
+#-------------------------------------
 # @ Chloé Stévenne    Victor Mangeleer
 
 python3 code/main.py
